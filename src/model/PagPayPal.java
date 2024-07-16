c2b9c2d7b09389f63c1e732c50a232924d669231
WilkerPhoenix <wilkerphoenix@gmail.com>
2024-07-16 17:19:06 -0300

    Interfaces novas

---
+++ src/model/PagPayPal.java
@@ -0,0 +1,7 @@
+public class PagPayPal implements Pagamento {
+    @Override
+    public void processarPagamento(double quantia) {
+        // falta
+        System.out.println("Pagamento de " + quantia + " processado via PayPal.");
+    }
+}
