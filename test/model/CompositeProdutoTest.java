cddd5e5aa0064363bd5fa269cb2f03fc9ecf468f
Ckdylon <87543997+Ckdylon@users.noreply.github.com>
2024-07-20 22:53:28 -0300

    Update CompositeProdutoTest.java

--- test/model/CompositeProdutoTest.java
+++ test/model/CompositeProdutoTest.java
@@ -6,43 +6,53 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CompositeProdutoTest {
-
+    private CompositeProduto compositeEletronico;
+    private CompositeProduto compositeRoupa;
+    private Produto celular;
+    private Produto camiseta;
+    private Produto calca;
 
     @BeforeEach
     void setUp() {
-        compositeRoupa = new CompositeProduto("Roupa", 0 ,0);
-        compositeEletronico = new Composite("Eletrônico", 0, 0);
+        compositeEletronico = new CompositeProduto("Eletrônico", 0, 0);
+        compositeRoupa = new CompositeProduto("Roupa", 0, 0);
         celular = ProdutoFactory.criarProduto("eletronico", "Celular", 1200.00, 10);
         camiseta = ProdutoFactory.criarProduto("roupa", "Camiseta", 50.00, 20);
-        camiseta = ProdutoFactory.criarProduto("roupa", "Calça", 50.00, 20);
-
-
+        calca = ProdutoFactory.criarProduto("roupa", "Calça", 80.00, 15);
     }
 
     @Test
     void addProduto() {
-
+        compositeEletronico.addProduto(celular);
+        assertEquals(1, compositeEletronico.getQuantidadeCarrinho());
     }
 
     @Test
     void getQuantidadeCarrinho() {
-        compositeRoupa = new CompositeProduto("Roupa", 0, 0);
-        compositeRoupa.
+        compositeRoupa.addProduto(camiseta);
+        compositeRoupa.addProduto(calca);
+        assertEquals(2, compositeRoupa.getQuantidadeCarrinho());
     }
 
     @Test
     void removeProduto() {
-    }
-
-    @Test
-    void testGetQuantidadeCarrinho() {
+        compositeRoupa.addProduto(camiseta);
+        compositeRoupa.addProduto(calca);
+        compositeRoupa.removeProduto(camiseta);
+        assertEquals(1, compositeRoupa.getQuantidadeCarrinho());
     }
 
     @Test
     void testAddProduto() {
+        compositeEletronico.addProduto(celular);
+        assertEquals(1, compositeEletronico.getQuantidadeCarrinho());
     }
 
     @Test
     void testRemoveProduto() {
+        compositeRoupa.addProduto(camiseta);
+        compositeRoupa.addProduto(calca);
+        compositeRoupa.removeProduto(camiseta);
+        assertEquals(1, compositeRoupa.getQuantidadeCarrinho());
     }
 }
