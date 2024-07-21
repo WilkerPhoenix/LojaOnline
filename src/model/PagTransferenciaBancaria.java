9ccb4db0e92e2cded3bc98718690f408924aba78
Ckdylon <87543997+Ckdylon@users.noreply.github.com>
2024-07-20 21:41:51 -0300

    Update PagTransferenciaBancaria.java

--- src/model/PagTransferenciaBancaria.java
+++ src/model/PagTransferenciaBancaria.java
@@ -2,8 +2,7 @@ package model;
 
 public class PagTransferenciaBancaria implements Pagamento {
     @Override
-    public void processarPagamento(double quantia) {
-        //esta faltando implemetar algo
-        System.out.println("Pagamento de " + quantia + " processado via Transferência Bancária.");
+    public boolean processarPagamento(double quantia, String nome, String dadosPagamento) {
+        return true;  
     }
 }
