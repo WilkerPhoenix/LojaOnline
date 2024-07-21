5cde86de101bfad0b660597fe86f3e77010049ad
Ckdylon <87543997+Ckdylon@users.noreply.github.com>
2024-07-20 21:43:25 -0300

    Update PagCartaoCredito.java

--- src/model/PagCartaoCredito.java
+++ src/model/PagCartaoCredito.java
@@ -2,10 +2,7 @@ package model;
 
 public class PagCartaoCredito implements Pagamento {
     @Override
-    public void processarPagamento(double quantia) {
-        // falta e como vai ser essa implementação?
-        System.out.println("Pagamento de " + quantia + " processado via Cartão de Crédito.");
+    public boolean processarPagamento(double quantia, String nome, String dadosPagamento) {
+        return true;
     }
 }
-
-
