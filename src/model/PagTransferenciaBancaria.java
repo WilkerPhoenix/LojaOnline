f8c51786a8720feef185738a4601ab4911b47728
Ckdylon <87543997+Ckdylon@users.noreply.github.com>
2024-07-20 21:49:31 -0300

    Update PagTransferenciaBancaria.java

--- src/model/PagTransferenciaBancaria.java
+++ src/model/PagTransferenciaBancaria.java
@@ -1,5 +1,5 @@
 package model;
-
+// colocar  na  interface uma verificação ´para ver se o usuario inseriu nome e dados de pagamento, para poder concluir o pagamento.
 public class PagTransferenciaBancaria implements Pagamento {
     @Override
     public boolean processarPagamento(double quantia, String nome, String dadosPagamento) {
