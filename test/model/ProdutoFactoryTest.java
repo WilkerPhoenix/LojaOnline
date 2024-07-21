e52829cc4782e1e71c8d7d39c8d303d2bd734c9b
WilkerPhoenix <wilkerphoenix@gmail.com>
2024-07-20 22:56:34 -0300

    Testes iniciais

--- test/model/ProdutoFactoryTest.java
+++ test/model/ProdutoFactoryTest.java
@@ -1,12 +1,24 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProdutoFactoryTest {
+    private ProdutoFactory produtoFactory;
+
+    @BeforeEach
+    void setUp() {
+        produtoFactory = new ProdutoFactory();
+    }
 
     @Test
     void criarProduto() {
+        produtoFactory.
+    }
+
+    @Test
+    void testCriarProduto() {
     }
 }
