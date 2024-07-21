ecc70870b3fb0704a28bbc8d88febc3484bdb831
WilkerPhoenix <wilkerphoenix@gmail.com>
2024-07-21 00:12:54 -0300

    Testes Iniciais

---
+++ test/model/PedidoTest.java
@@ -0,0 +1,39 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PedidoTest {
+    private Pedido pedido;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void proximoEstado() {
+    }
+
+    @Test
+    void estadoAnterior() {
+    }
+
+    @Test
+    void getEstado() {
+    }
+
+    @Test
+    void setEstado() {
+    }
+
+    @Test
+    void calcularPrecoTotal() {
+    }
+
+    @Test
+    void processarPagamento() {
+    }
+}
